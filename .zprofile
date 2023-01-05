bc7173ab0383535701a3a37ab865083c9687eb3f
Kawatsu Kenya <3982ne@gmail.com>
2023-01-05 18:55:17 +0900

    fix brew path

--- .zprofile
+++ .zprofile
@@ -1,3 +1,6 @@
+# homebrew installed packages
+export PATH="/opt/homebrew/bin:$PATH"
+
 # Homebrew
 # Set PATH, MANPATH, etc., for Homebrew.
 eval "$($(brew --prefix)/bin/brew shellenv)"
--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# homebrew installed packages
-export PATH="$(brew --prefix)/bin:$PATH"
-
 # git(https://github.com/git/git/blob/master/contrib/completion/git-completion.zsh)
 # allows you to see repository status in your prompt.
 [ -s "${HOME}/.git-prompt.sh" ] && . "${HOME}/.git-prompt.sh"
